d31f74b79aa00e54b3f36c64db4d045c72900866
Alden Ozburn <aldendino@gmail.com>
2018-02-11 11:24:57 -0500

    Bold hints in css for focus.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -148,6 +148,7 @@ export default {
   &.relative
     background lightblue
   &.original
+    font-weight bold
     color blue
     &.current
       color lightblue
@@ -164,6 +165,7 @@ export default {
   color pink
   font-size 25px
   .answer
+    font-weight bold
     color white
 
 // .sudoku-board
